f2c2f58f7df41c0cad3317e07f660ef0ffd9be5c
JustintyC <justin.ty.cheung@gmail.com>
2025-03-21 00:11:13 -0700

    updated parsing of workday's api response

--- src/CourseList/courseListUtils.js
+++ src/CourseList/courseListUtils.js
@@ -1,4 +1,21 @@
 // calls workday's api and returns advanced course info as a json
+/**
+ * 
+ * @param {*} url 
+ * @returns 
+        status,
+        startEndDate,
+        grading,
+        description,
+        otherFormats,
+        courseTags,
+        availUnreserved,
+        availReserved,
+        meetingPatterns,
+        instructors,
+        reservedDist,
+        notes
+ */
 export async function fetchMoreInfo(url) {
     const out = await fetch(url)
         .then(response => {
@@ -7,99 +24,115 @@ export async function fetchMoreInfo(url) {
             }
             return response.json();
         }).then(data => {
-            /*
-            contains:
-            - academic period (doesn't show multi-term courses)
-            - status (open/closed/waitlist)
-            - start/end date (use this to find multi-term courses)
-            - grading basis (credit/d/fail, graded)
-            - description, which contains
-                - prereqs
-            - other instructional formats (labs/discussions)
-
-            */
-            const firstArr = data["body"]["children"][0]["children"][0]["children"];            
-            
             let status = null;
             let startEndDate = null;
             let grading = null;
             let description = null;
             let otherFormats = null;
-
-            for (let json of firstArr) {
-                switch(json.label) {
-                    case "Status":
-                        status = json.instances && json.instances[0] && json.instances[0].text;
-                        break;
-                    case "Start/End Date":
-                        startEndDate = json.value;
-                        break;
-                    case "Grading Basis":
-                        grading = json.instances && json.instances.map(instance => instance.text);
-                        break;
-                    case "Description":
-                        description = json.value.replace(/<\/?[a-zA-Z]+>/g, "");
-                        break;
-                    case "Other Instructional Formats":
-                        otherFormats = json.instances && json.instances.map(instance => instance.text);
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            /*
-            contains:
-            - instructors
-            - course tags (course level, credit/d/fail, etc)
-            - detailed seat info
-                - avaiable unreserved seats
-                - available reserved seats
-                - reserved seats distribution
-            - full meeting pattern text
-            - notes
-
-            */
-            const secondArr = data["body"]["children"][0]["children"][1]["children"];
-
             let courseTags = null;
             let availUnreserved = null;
             let availReserved = null;
             let meetingPatterns = null;
             let instructors = null;
-            let reservedDist = null;
+            let reservedDist = [];
             let notes = null;
+            let totalCapacity = null;
 
-            for (let json of secondArr) {
-                switch (json.label) {
-                    case "Course Tags":
-                        courseTags = json.instances && json.instances.map(instance => instance.text);
+            /*
+            generalInformationArr: contains
+            - course info (code, title)
+            - description, which contains prereqs/coreqs
+            - academic period (e.g. "2024-25 Winter Term 2 (UBC-V)")
+            - instructors
+            - start/end date (e.g. "2025-01-06 - 2025-04-08")
+            - status (open/closed etc)
+            - total section capacity (as string and int)
+            - unreserved seats available (e.g. "4 of 45")
+            - reserved seats available (e.g. "13 of 152")
+            */
+            const generalInformationArr = data["body"]["children"][0]["sections"][0]["children"][0]["children"];
+
+            /*
+            reservedDistArr: contains reserved seats distribution, requires further parsing
+            */
+            const reservedDistArr = data["body"]["children"][0]["sections"][0]["children"][1]?.["rows"];
+
+            /*
+            additionalDetailsArr: contains
+            - credits (e.g. "4 Credits")
+            - grading basis (C/D/F, Graded, etc)
+            - waitlist spots (if available)
+            - instructional format (lecture, lab, etc)
+            - other instructional formats (labs/discussions)
+            - delivery mode
+            - campus
+            - full meeting pattern text
+            - course tags (course level, credit/d/fail, etc)
+            - notes (if available)
+            */
+            const additionalDetailsArr = data["body"]["children"][0]["sections"][1]["children"][0]["children"][0]["children"][0]["children"];
+
+            for (let json of generalInformationArr) {
+                switch(json.label) {
+                    case "Description":
+                        description = json.value.replace(/<\/?[a-zA-Z]+>/g, "");
                         break;
-                    case "Available Unreserved Seats":
+                    case "Start/End Date":
+                        startEndDate = json.value;
+                        break;
+                    case "Status":
+                        status = json.instances[0].text;
+                        break;
+                    case "Total Section Capacity":
+                        totalCapacity = json.text;
+                        break;
+                    case "Unreserved Seats Available":
                         availUnreserved = json.value;
                         break;
-                    case "Available Reserved Seats":
+                    case "Reserved Seats Available":
                         availReserved = json.value;
+                        break;
+                    default:
+                        if (json.widget && json.widget === "panel") {
+                            // instructors: only json with widget = panel
+                            const instructorsArr = json.children?.[0]?.instances;
+                            if (instructorsArr) {
+                                instructors = instructorsArr.map(instructorJson => instructorJson.text);
+                            }
+                        }
+                        break;
+                }
+            }
+
+            for (let json of additionalDetailsArr) {
+                switch(json.label) {
+                    case "Grading Basis":
+                        grading = json.instances[0].text;
+                        break;
+                    case "Other Instructional Formats":
+                        otherFormats = json.instances[0].text;
                         break;
                     case "Meeting Patterns":
                         meetingPatterns = json.instances && json.instances.map(instance => instance.text);
                         break;
+                    case "Course Tags":
+                        courseTags = json.instances && json.instances.map(instance => instance.text);
+                        break;
                     case "Notes":
                         notes = json.value.replace(/<\/?[a-zA-Z]+>/g, "");
                         break;
-                    default: // 2 options not identifiable by label: instructors and reserved seats distribution
-                        if (json.widget && json.widget === "panel") {
-                            // instructors: only json with widget = panel
-                            const instructorsArr = json.children?.[0]?.children?.[0]?.instances;
-                            if (instructorsArr) {
-                                instructors = instructorsArr.map(instructorJson => instructorJson.text);
-                            }
-                        } else if (json.propertyName.includes("Reserved_Capacity_Line_Student")) {
-                            // reserved seats distribution: propertyName = wd:Student_Course_Section_Component__Nonsingular_--IS
-                            reservedDist = json.instances && json.instances.map(instance => instance.text);
-                        }
-
+                    default:
                         break;
+                    
+                }
+            }
+
+            if (reservedDistArr) {
+                for (let i = 0; i < reservedDistArr.length - 1; i++) {
+                    const cellsMap = reservedDistArr[i].cellsMap;
+                        // Available of Capacity - Eligibility
+                        const parsedText = `${cellsMap["201.2"].text} of ${cellsMap["201.3"].text} - ${cellsMap["201.1"].instances[0]["text"].split("-")[1].trim()}`;
+                        reservedDist.push(parsedText);
                 }
             }
             
